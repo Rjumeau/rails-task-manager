963ed358400a27ea5e4018c7da854f0a0c6a0559
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-15 14:38:20 +0200

    controller and view refacto done

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  # root 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
   root 'tasks#index'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks, only: [:show, :new, :create, :edit, :update, :destroy]
 end
