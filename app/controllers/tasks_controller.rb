d58ede858f0f2fc93336f176776844308efb0231
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-15 14:22:26 +0200

    update and delete method done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,13 @@
 
 # This controller handle tasks actions
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -20,7 +20,23 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
 
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
